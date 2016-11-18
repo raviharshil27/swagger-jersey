807f233834a07840158379bbc4c8728635c7b248
Harshil Shah <harshil.shah@itsoninc.com>
2016-11-18 01:43:37 -0800

    working commit

---
+++ src/main/java/com/howtodoinjava/jersey/Bootstrap.java
@@ -0,0 +1,27 @@
+package com.howtodoinjava.jersey;
+
+import io.swagger.jaxrs.config.BeanConfig;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public class Bootstrap extends HttpServlet {
+  /**
+   *
+   * @param config
+   * @throws ServletException
+   */
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    super.init(config);
+
+    BeanConfig beanConfig = new BeanConfig();
+    beanConfig.setVersion("1.0.2");
+    beanConfig.setSchemes(new String[]{"http"});
+    beanConfig.setHost("localhost:8080");
+    beanConfig.setBasePath("/JerseyDemos");
+    beanConfig.setResourcePackage("io.swagger.resources,com.howtodoinjava.jersey");
+    beanConfig.setScan(true);
+  }
+}
